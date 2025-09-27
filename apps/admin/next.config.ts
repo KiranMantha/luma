25051ef92ce4389404ae0f7c8c83a2b5a69cf76b
KiranMantha <kiranv.mantha@gmail.com>
2025-09-27 15:25:49 +0530

    feat: updated nextconfig for standalone build. added node script to simulate prod deployment

--- apps/admin/next.config.ts
+++ apps/admin/next.config.ts
@@ -1,10 +1,13 @@
 import type { NextConfig } from 'next';
+import path from 'path';
 
 const nextConfig: NextConfig = {
   distDir: 'dist',
   output: 'standalone',
   transpilePackages: ['@repo/ui'],
   reactStrictMode: true,
+  // adjust this so it points to your repository root (where your workspace package.json is)
+  outputFileTracingRoot: path.join(__dirname, '../../'), // <-- example for apps/web -> repoRoot
 };
 
 export default nextConfig;
