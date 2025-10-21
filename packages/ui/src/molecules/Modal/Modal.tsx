d75b9bb301de6dac06c1448b88f59ace0cd2c987
KiranMantha <kiranv.mantha@gmail.com>
2025-10-21 18:04:53 +0530

    Updated modal ui

--- packages/ui/src/molecules/Modal/Modal.tsx
+++ packages/ui/src/molecules/Modal/Modal.tsx
@@ -1,7 +1,9 @@
+import { Box } from '#atoms';
 import { Dialog, DialogPanel, DialogTitle, Transition, TransitionChild } from '@headlessui/react';
 import { clsx } from 'clsx';
 import { Fragment } from 'react';
 import { ModalProps } from './Modal.model';
+import styles from './Modal.module.scss';
 
 export const Modal = ({ open, onOpenChange, title, children, size = 'md' }: ModalProps) => {
   const sizeClasses = {
@@ -13,7 +15,7 @@ export const Modal = ({ open, onOpenChange, title, children, size = 'md' }: Moda
 
   return (
     <Transition appear show={open} as={Fragment}>
-      <Dialog as="div" className="relative z-50" onClose={() => onOpenChange(false)}>
+      <Dialog as="div" className={styles.modal} onClose={() => onOpenChange(false)}>
         <div className="fixed inset-0 overflow-y-auto">
           <div className="flex min-h-full items-center justify-center bg-black/30 p-4 text-center">
             <TransitionChild
@@ -27,17 +29,14 @@ export const Modal = ({ open, onOpenChange, title, children, size = 'md' }: Moda
             >
               <DialogPanel
                 className={clsx(
-                  'w-full transform overflow-hidden rounded-md bg-white p-6 text-left align-middle shadow-xl transition-all',
+                  'w-full transform overflow-hidden rounded-md bg-white text-left align-middle shadow-xl transition-all',
                   sizeClasses[size],
                 )}
               >
-                {title && (
-                  <DialogTitle as="h3" className="mb-4 text-lg leading-6 font-medium text-gray-900">
-                    {title}
-                  </DialogTitle>
-                )}
-
-                <div className="mt-2">{children}</div>
+                <DialogTitle as="h3" className={styles.title}>
+                  {title}
+                </DialogTitle>
+                <Box className="p-4">{children}</Box>
               </DialogPanel>
             </TransitionChild>
           </div>
